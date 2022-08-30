17a9aaf7e4937b78113e3e878f1ebcebf31fe565
Angel prajapati <89209140+anhelpr@users.noreply.github.com>
2022-08-30 14:45:37 +0530

    Add files via upload

---
+++ chate_app_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDtyaFe4v-K9R7mNgKdi9LYk9EVz_dgW2o",
+    authDomain: "kwitter-3b703.firebaseapp.com",
+    databaseURL: "https://kwitter-3b703-default-rtdb.firebaseio.com",
+    projectId: "kwitter-3b703",
+    storageBucket: "kwitter-3b703.appspot.com",
+    messagingSenderId: "784851417258",
+    appId: "1:784851417258:web:e067e2feea59fbc68638d3"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
